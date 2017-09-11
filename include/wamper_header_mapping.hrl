7684a6c25a6363a394ae99ca9c0b69a3b43db074
Federico Repond <frepond@gmail.com>
2017-09-11 18:24:47 -0300

    Unmap errors

--- include/wamper_header_mapping.hrl
+++ include/wamper_header_mapping.hrl
@@ -27,21 +27,21 @@
 
 -define(ERROR_MAPPING, [
   {},
-  {goodbye_and_out, <<"wamp.error.goodbye_and_out">>},
-  {authorization_failed, <<"wamp.error.authorization_failed">>},
-  {canceled, <<"wamp.error.canceled">>},
-  {close_realm, <<"wamp.error.close_realm">>},
-  {disclose_not_allowed, <<"wamp.error.disclose_me.not_allowed">>},
-  {invalid_argument, <<"wamp.error.invalid_argument">>},
-  {invalid_uri, <<"wamp.error.invalid_uri">>},
-  {no_such_procedure, <<"wamp.error.no_such_procedure">>},
-  {no_such_realm, <<"wamp.error.no_such_realm">>},
-  {no_such_registration, <<"wamp.error.no_such_registration">>},
-  {no_such_role, <<"wamp.error.no_such_role">>},
-  {no_such_subscription, <<"wamp.error.no_such_subscription">>},
-  {not_authorized, <<"wamp.error.not_authorized">>},
-  {procedure_already_exists, <<"wamp.error.procedure_already_exists">>},
-  {system_shutdown, <<"wamp.error.system_shutdown">>}
+  %% {goodbye_and_out, <<"wamp.error.goodbye_and_out">>},
+  %% {authorization_failed, <<"wamp.error.authorization_failed">>},
+  %% {canceled, <<"wamp.error.canceled">>},
+  %% {close_realm, <<"wamp.error.close_realm">>},
+  %% {disclose_not_allowed, <<"wamp.error.disclose_me.not_allowed">>},
+  %% {invalid_argument, <<"wamp.error.invalid_argument">>},
+  %% {invalid_uri, <<"wamp.error.invalid_uri">>},
+  %% {no_such_procedure, <<"wamp.error.no_such_procedure">>},
+  %% {no_such_realm, <<"wamp.error.no_such_realm">>},
+  %% {no_such_registration, <<"wamp.error.no_such_registration">>},
+  %% {no_such_role, <<"wamp.error.no_such_role">>},
+  %% {no_such_subscription, <<"wamp.error.no_such_subscription">>},
+  %% {not_authorized, <<"wamp.error.not_authorized">>},
+  %% {procedure_already_exists, <<"wamp.error.procedure_already_exists">>},
+  %% {system_shutdown, <<"wamp.error.system_shutdown">>}
 ]).
 
 -define(HELLO_MAPPING, [
